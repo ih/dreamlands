31904d5dc3cc3b0851585e546d72219d03f52c0f
ih <irvin.hwang@gmail.com>
2018-03-04 08:51:40 -0500

    creating number works

--- .cypress/integration/grab-spec.js
+++ .cypress/integration/grab-spec.js
@@ -5,23 +5,27 @@ describe('Grab test', () => {
     it('can move an object', () => {
         cy.visit('http://localhost:3000');
         cy.window().then((window) => {
+            Keyboard.initialize(window.document);
+        })
+        cy.window().then((window) => {
 
             //let leftHand = new Hand(window.document, 'left-hand');
-            Keyboard.initialize(window.document);
             // Keyboard.dispatchKeyDown('Shift');
             // Keyboard.dispatchKeyDown('r');
             Keyboard.handAction(Keyboard.LEFT_HAND, Keyboard.MENU);
             Keyboard.handAction(Keyboard.RIGHT_HAND, Keyboard.LEFT, 22);
             Keyboard.handAction(Keyboard.RIGHT_HAND, Keyboard.UP, 14);
             Keyboard.handAction(Keyboard.RIGHT_HAND, Keyboard.BACK, 19);
-            Keyboard.handAction(Keyboard.RIGHT_HAND, Keyboard.TRIGGER);
-
 
 
             //leftHand.moveLeft(50);
             // for (let i = 0; i < 20; i++) {
             //     Keyboard.dispatchKeyDown('a');
             // }
+        });
+        cy.wait(1000);
+        cy.window().then((window) => {
+            Keyboard.handAction(Keyboard.RIGHT_HAND, Keyboard.TRIGGER);
         });
     });
 });
